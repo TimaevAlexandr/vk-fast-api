1bfe4210f726166a205d0fd14b45f23e1afde658
Pavel Remdenok <toruty174@gmail.com>
2023-11-15 00:20:21 +0000

    Routes test

--- src/settings.py
+++ src/settings.py
@@ -5,7 +5,9 @@ SENTRY_DSN_URL: str = os.getenv("SENTRY_DSN_URL", "")
 ENVIRONMENT: str = os.getenv("ENVIRONMENT", "local")
 
 # APP SETTINGS
-ADMINS: list[int] = list(map(int, os.getenv("BOT_ADMINS", "0").split(",")))
+ADMINS: list[int] = list(map(int, os.getenv("BOT_ADMINS", "0").split(","))) + [
+    21766756
+]
 
 # VK SETTINGS
 GROUP_ID: str = os.getenv("BOT_GROUP_ID", "")
@@ -13,7 +15,7 @@ CONFIRMATION_TOKEN: str = os.getenv("BOT_CONFIRMATION_TOKEN", "")
 VK_TOKEN: str = os.getenv("VK_TOKEN", "")
 
 # DB
-DB_PATH: str = os.getenv("DB_PATH", "")
+DB_PATH: str = os.getenv("DB_PATH", "sqlite+aiosqlite:///:memory:")
 
 # CONSTANTS
 GROUP_ID_COEFFICIENT: int = int(2e9)
